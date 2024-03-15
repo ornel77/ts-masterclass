aeaaba3a8ba689704d76ee84148110a2fd7d6dd8
ornel77 <t.ornel@yahoo.fr>
2024-03-15 14:50:24 +0100

    first 12 lessons ok

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/* ---------------------------------- Array --------------------------------- */
+let names: string[] = ['hello', 'world']
+let ages: number[] = [23, 24, 12]
+names.push("Bowser")
+ages.push(1)
+console.log(ages);
+
+/* ----------------------------- Object literals ---------------------------- */
+
+/* ------------------- Type inference with object literal ------------------- */
