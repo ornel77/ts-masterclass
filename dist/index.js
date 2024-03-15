1a5eff0c5054c0e2765eae96f420786c42b369f6
ornel77 <t.ornel@yahoo.fr>
2024-03-15 23:54:17 +0100

    intersection types, end 26

--- dist/index.js
+++ dist/index.js
@@ -1,39 +1,7 @@
 "use strict";
-/* -------------------------------------------------------------------------- */
-/*                                Generics 101                                */
-Object.defineProperty(exports, "__esModule", { value: true });
-/* -------------------------------------------------------------------------- */
-function logAndReturnString(val) {
-    console.log(val);
-    return val;
+function addIdToValue(val) {
+    const id = Math.random();
+    return Object.assign(Object.assign({}, val), { id });
 }
-function logAndReturnNumber(val) {
-    console.log(val);
-    return val;
-}
-function logAndReturnBoolean(val) {
-    console.log(val);
-    return val;
-}
-// annoying to do 3 function that does the same type so we use generics : 
-// Example 1
-function logAndReturnValue(val) {
-    console.log(val);
-    return val;
-}
-const result = logAndReturnValue('mario');
-const result1 = logAndReturnValue(1);
-const result2 = logAndReturnValue(false);
-// Example 2
-function getRandomArrayValue(values) {
-    const i = Math.floor(Math.random() * values.length);
-    return values[i];
-}
-const users = [
-    { name: 'mario', score: 100 },
-    { name: 'peach', score: 150 },
-    { name: 'wario', score: 20 },
-    { name: 'yoshi', score: 90 },
-];
-const randomUser = getRandomArrayValue(users);
-console.log(randomUser);
+const post = addIdToValue({ title: 'Marmite Rules', thumbsUp: 521 });
+console.log(post);
--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,20 @@
 /* -------------------------------------------------------------------------- */
-/*                                Generics 101                                */
-
-import { getRandomValues } from "crypto";
-
+/*                              Intersection Type                             */
 /* -------------------------------------------------------------------------- */
-function logAndReturnString(val: string): string {
-  console.log(val);
-  return val;
-}
-function logAndReturnNumber(val: number): number {
-  console.log(val);
-  return val;
-}
-function logAndReturnBoolean(val: boolean): boolean {
-  console.log(val);
-  return val;
+interface HasID {
+  id: number
 }
 
-// annoying to do 3 function that does the same type so we use generics : 
-// Example 1
-function logAndReturnValue<T>(val: T): T {
-  console.log(val);
-  return val
+function addIdToValue<T>(val: T): T & HasID {
+  const id = Math.random()
+
+  return {...val, id}
+}
+// this is T
+interface Post {
+  title: string
+  thumbsUp: number
 }
 
-const result = logAndReturnValue<string>('mario')
-const result1 = logAndReturnValue<number>(1)
-const result2 = logAndReturnValue<boolean>(false)
-
-// Example 2
-function getRandomArrayValue<T>(values: T[]): T {
-  const i = Math.floor(Math.random() * values.length)
-  return values[i]
-}
-
-interface User {
-  name: string,
-  score: number
-}
-
-const users: User[] = [
-  {name: 'mario', score: 100},
-  {name: 'peach', score: 150},
-  {name: 'wario', score: 20},
-  {name: 'yoshi', score: 90},
-]
-
-const randomUser = getRandomArrayValue<User>(users)
-console.log(randomUser);
+const post = addIdToValue<Post>({title: 'Marmite Rules', thumbsUp: 521})
+console.log(post);
