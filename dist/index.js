8ececed282999d0818e234cc50b156a1494ca174
ornel77 <t.ornel@yahoo.fr>
2024-03-15 17:08:50 +0100

    abstract end 22

--- dist/index.js
+++ dist/index.js
@@ -22,24 +22,40 @@ class Pizza extends MenuItem {
         this.toppings.push(topping);
     }
     removeTopping(topping) {
-        this.toppings = this.toppings.filter(t => t !== topping);
+        this.toppings = this.toppings.filter((t) => t !== topping);
     }
     selectBase(b) {
         this.base = b;
     }
+    format() {
+        let formatted = this.details + '\n';
+        // base
+        formatted += `Pizza on a ${this.base} base`;
+        // toppings
+        if (this.toppings.length < 1) {
+            formatted += 'with no toppings';
+        }
+        else {
+            formatted += ` with ${this.toppings.join(', ')}`;
+        }
+        return formatted;
+    }
 }
 const pizza = new Pizza('mario special', 15);
 const pizza2 = new Pizza('luigi special', 17);
-// function addMushroomsToPizzas(pizzas: Pizza[]): void {
-//   pizzas.forEach((pizza: Pizza) => {
-//     pizza.addTopping('mushrooms')
-//   })
-// }
-// addMushroomsToPizzas([pizza, pizza2])
+function addMushroomsToPizzas(pizzas) {
+    pizzas.forEach((pizza) => {
+        pizza.addTopping('mushrooms');
+    });
+}
+addMushroomsToPizzas([pizza, pizza2]);
 // function printMenuItem(item: MenuItem): void {
 //   console.log(item.details);
 // }
 // printMenuItem(pizza)
+pizza.addTopping('olives');
+pizza.addTopping('jam');
+pizza.addTopping('burrata');
 function printFormatted(val) {
     console.log(val.format());
 }
--- src/index.ts
+++ src/index.ts
@@ -2,61 +2,79 @@
 type Base = 'classic' | 'thick' | 'thin' | 'garlic';
 
 interface HasFormatter {
-  format(): string
+  format(): string;
 }
 
 // inheretance
-class MenuItem implements HasFormatter {
-  constructor(private title: string,private price: number) {
-    
-  }
+abstract class MenuItem implements HasFormatter {
+  constructor(private title: string, private price: number) {}
   get details(): string {
-    return `${this.title} - £${this.price}`
+    return `${this.title} - £${this.price}`;
   }
 
-  format(): string {
-    return `this menu item is called ${this.title} and is £${this.price}`
+  abstract format(): string {
+    return `this menu item is called ${this.title} and is £${this.price}`;
   }
 }
 
-class Pizza extends MenuItem{
-  constructor(title: string,price: number) {
-    super(title, price)
+class Pizza extends MenuItem {
+  constructor(title: string, price: number) {
+    super(title, price);
   }
-  private base: Base = 'classic'
-  private toppings: string[] = []
+  private base: Base = 'classic';
+  private toppings: string[] = [];
 
   addTopping(topping: string): void {
-    this.toppings.push(topping)
-  } 
+    this.toppings.push(topping);
+  }
 
   removeTopping(topping: string): void {
-    this.toppings = this.toppings.filter(t => t !== topping)
+    this.toppings = this.toppings.filter((t) => t !== topping);
   }
 
   selectBase(b: Base): void {
-    this.base = b
+    this.base = b;
+  }
+
+  format(): string {
+    let formatted = this.details + '\n';
+
+    // base
+    formatted += `Pizza on a ${this.base} base`;
+
+    // toppings
+    if (this.toppings.length < 1) {
+      formatted += 'with no toppings';
+    } else {
+      formatted += ` with ${this.toppings.join(', ')}`;
+    }
+
+    return formatted;
   }
 }
 
 const pizza: Pizza = new Pizza('mario special', 15);
 const pizza2 = new Pizza('luigi special', 17);
 
-// function addMushroomsToPizzas(pizzas: Pizza[]): void {
-//   pizzas.forEach((pizza: Pizza) => {
-//     pizza.addTopping('mushrooms')
-//   })
-// }
+function addMushroomsToPizzas(pizzas: Pizza[]): void {
+  pizzas.forEach((pizza: Pizza) => {
+    pizza.addTopping('mushrooms')
+  })
+}
 
-// addMushroomsToPizzas([pizza, pizza2])
+addMushroomsToPizzas([pizza, pizza2])
 // function printMenuItem(item: MenuItem): void {
 //   console.log(item.details);
 // }
 
 // printMenuItem(pizza)
 
+pizza.addTopping('olives')
+pizza.addTopping('jam')
+pizza.addTopping('burrata')
+
 function printFormatted(val: HasFormatter): void {
   console.log(val.format());
 }
 
-printFormatted(pizza)
+printFormatted(pizza);
