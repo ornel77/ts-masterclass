0f031a11e1340221e5229d70b4390b1224c2e697
ornel77 <t.ornel@yahoo.fr>
2024-03-16 01:18:47 +0100

    did lesson33 adding parcel

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/* ---------------------------------- Array --------------------------------- */
-let names = ['hello', 'world'];
-let ages = [23, 24, 12];
-names.push("Bowser");
-ages.push(1);
-console.log(ages);
-/* ----------------------------- Object literals ---------------------------- */
-/* ------------------- Type inference with object literal ------------------- */ 
